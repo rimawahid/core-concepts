1690bdbd8d05a480b7e22980b4c57ecd3a96c797
Rima Akter <rimawahid3@gmail.com>
2021-02-23 00:21:14 +0600

    jsx

--- src/App.js
+++ src/App.js
@@ -2,10 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var person ={
+    name: 'John',
+    age: 34
+  }
+  var person2 = {
+    name: "Rahim",
+    age: 30
+  }
+  var styles = {
+    color: 'red',
+    backgroundColor: 'yellow'
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <header className="App-header">
+       <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,9 +29,25 @@ function App() {
         >
           Learn React
         </a>
+        
       </header>
+      {//try jsx
+      /* <h1>My Heading{(2+5)}</h1> */}
+      {/* <h1 className="" style={styles}>Name : {person.name + " " + person.age}</h1>
+      <h2 style = {{color:"white",backgroundColor:"blue"}}>Name : {person2.name + " " + person2.age}</h2> */}
+    
+
     </div>
   );
+}
+
+function player(){
+  return (
+    <div>
+      <h1>Name: Sakib Al Hasa</h1>
+      <h3>Hero of the year</h3>
+    </div>
+  )
 }
 
 export default App;
